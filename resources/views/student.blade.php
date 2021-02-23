33e64c02645a16da45873dfa655c82a8b5f12e5a
Nurbek <nurbek11187@gmail.com>
2021-02-23 16:04:26 +0600

    Add some web Views

---
+++ resources/views/student.blade.php
@@ -0,0 +1,1 @@
+<h1>sTUDENT ID <?php echo $id?></h1>
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,9 @@ Route::get('/about', 'MainController@about');
 Route::get('/article', 'MainController@article')->name('article');
 
 Route::post('/article/check', 'MainController@article_check');
+
+Route::resource('/student2', 'StudentsController');
+
+
+Route::get('/student', 'StudentsController@index');
+Route::get('/details/{id}', 'StudentsController@display');
